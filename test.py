4dc70ffe791986b5aa4df07d2383456e77564a66
Ruben classle <rubenraj@classle.co.in>
2017-11-09 10:53:54 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,21 @@
+from sklearn import tree
+
+#[height, weight, shoe size]
+
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,Y)
+prediction = clf.predict([[150, 50, 32]])
+print(prediction)
+
+# from sklearn import tree
+# X = [[0, 0], [1, 1]]
+# Y = [0, 1]
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(X, Y)
+# pred = clf.predict([[2., 2.]])
+# print(pred)
